5257f2986e5ff4a51246f5b1a982abad9a2a5b91
Nathcat <supernath126@gmail.com>
2022-04-22 22:59:17 +0100

    added heartbeat management

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const mailTransporter = nodemailer.createTransport({
     }
 });
 
+var heartbeatBuffer = [];
+PresenceManager();
+
 http.createServer(function (req, res) {
     // Parse the URL to check the requested action
     var path = url.parse(req.url, true).pathname;
@@ -455,6 +458,32 @@ http.createServer(function (req, res) {
             })
         })
     }
+    else if (path === "/api/heartbeat") {
+        if (req.method == "GET") {
+            res.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "text/html"
+            });
+    
+            return res.end("<h1>Invalid method</h1>");
+        }
+
+        res.writeHead(200, {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "text/plain"
+        });
+
+        let buffers = [];
+        req.on("data", (chunk) => {
+            buffers.push(chunk);
+        });
+
+        req.on("end", () => {
+            let data = JSON.parse(buffers.concat());
+            heartbeatBuffer.push(data);
+            return res.end("OK");
+        });
+    }
 
 }).listen(8080);
 
@@ -776,4 +805,52 @@ async function ApplyUserSettings(data, callback) {
         //if (err) throw err;
         return callback();
     });
+}
+
+async function PresenceManager() {
+    while (true) {
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "login",
+            password: "",
+            database: "messagecat",
+            multipleStatements: true
+        });
+
+        con.connect(function(err) {
+            //if (err) throw err;
+        });
+
+        let setActiveQuery = "UPDATE `user-active-states` SET active = 1 WHERE ";
+
+        for (let i = 0; i < heartbeatBuffer.length; i++) {
+            let newCondition = "";
+            if (i != 0) {
+                newCondition += " or ";
+            }
+
+            newCondition += "userID like " + heartbeatBuffer[i].ID;
+            setActiveQuery += newCondition;
+        }
+
+        let setInactiveQuery = "UPDATE `user-active-states` SET active = 0 WHERE ";
+    
+        for (let i = 0; i < heartbeatBuffer.length; i++) {
+            let newCondition = "";
+            if (i != 0) {
+                newCondition += " and ";
+            }
+
+            newCondition += "userID not like " + heartbeatBuffer[i].ID;
+            setActiveQuery += newCondition;
+        }
+
+        heartbeatBuffer = [];
+        
+        con.query(setInactiveQuery + "; " + setActiveQuery, function (err, result, fields) {
+            con.destroy();
+        });
+    }
 }
